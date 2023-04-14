aa4bcfffadf1fc15bedf945df37c8d6c3a777f7b
aymen895 <101347192+aymen895@users.noreply.github.com>
2023-04-14 14:41:04 +0100

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -365,7 +365,7 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href=""
           target="_blank"
           rel="noreferrer"
           className="underline"
